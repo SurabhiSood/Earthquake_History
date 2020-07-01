ba14d9502e4c7196c33a274e13e19712f81aaa2d
Sur Sood <soodsur007@gmail.com>
2020-06-30 22:44:52 -0400

    code

--- logic.js
+++ logic.js
@@ -1,6 +1,13 @@
 //obtain the url from API Documentation-EQ Catalog [https://earthquake.usgs.gov/fdsnws/event/1/]
 //create the query with desired method and perimeters(format,lat,long,start and end date)
 
-var url = "https://earthquake.usgs.gov/fdsnws/event/1/application.json?format=geojson&starttime=2014-01-01&endtime=" +
+var url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=" +
 "2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
+// performing GET request to read the data
+d3.json(url, function(data){
+  console.log(data);
+})
 
+function createFeatures(earthquake){
+  
+}
